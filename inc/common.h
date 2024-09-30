a39490fc55418157714773145c2e73d60efde241
SergALLy <cfyz70903@mail.ru>
2024-09-30 22:20:02 +0300

    Add inc ana src

---
+++ inc/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+// Общие библиотеки
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// Пользовательские библиотеки
+#include "encoder.h" // Работа с энкодером и кнопкой
+#include "sem_ind.h" // Работа с матрицей семисегментных индикаторов
+
+#endif
