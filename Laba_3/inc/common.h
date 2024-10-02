bba8dcfcb0412d74f28deb519628a9593190045c
SergALLy <cfyz70903@mail.ru>
2024-10-02 20:52:04 +0300

    add Laba_3

---
+++ Laba_3/inc/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+// Общие библиотеки
+#include <avr/io.h>
+#include <util/delay.h>
+//#include <avr/interrupt.h>
+
+// Пользовательские библиотеки
+#include "rgb_led.h" // Работа с RGB-светодиодом
+
+#endif
