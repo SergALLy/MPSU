7f51df5e9936625f1e0a28b730911f871b691ec7
SergALLy <cfyz70903@mail.ru>
2024-10-15 21:08:26 +0300

    add Laba 5

---
+++ Laba_5/inc/rangefinder.h
@@ -0,0 +1,29 @@
+#ifndef __RANGEFINDER_H__
+#define __RANGEFINDER_H__
+
+//INCLUDE
+#include "common.h"
+//-------------------------------------------------------
+
+//DEFINE
+#define TRIG 6
+#define ECHO_PIN 7
+#define ECHO INT7
+// Настройка прерываний
+#define INTERRUPT_RANGEFINDER EICRB |= (1<<ISC70); // Любое изменение на INT7
+/*
+    Нормальный, OCR1N отключён, предделитель на 256
+    Инициализация таймера No1.
+    Нормальный режим, OCR1N отключён,
+    преддедитель на 256
+*/
+#define RANGEFINDER_SETTING_A TCCR3A=TCCR3A
+#define RANGEFINDER_SETTING_B TCCR1B = (1<<CS12);
+
+//-------------------------------------------------------
+
+//FUNCTION
+void init_rangefinder();
+uint16_t listen_rangefinder();
+//-------------------------------------------------------
+#endif
---
+++ Laba_5/src/rangefinder.c
@@ -0,0 +1,47 @@
+#include "..\inc\rangefinder.h"
+
+uint16_t dist=0;
+
+void init_rangefinder()
+{
+    // Настройка прерываний
+    cli();
+    INTERRUPT_RANGEFINDER; // Любое изменение на INT7
+    EIMSK |= (1<<ECHO); // Разрешить локально прерывание на INT7
+    sei(); // Разрешить прерывание глобально
+    // Настройка таймера 1
+    RANGEFINDER_SETTING_A;
+    RANGEFINDER_SETTING_B;
+    // Настройка ног
+    DDRE |= (1<<TRIG);
+}
+
+ISR(INT7_vect)
+{
+    // если фронт сигнала на ECHO, то сбрасываем таймер
+    if((PINE& (1<<ECHO_PIN)) != 0)
+    {
+        TCNT1H= 0;
+        TCNT1L= 0;
+    } 
+    else
+    { // если срез сигнала на ECHO, то забираем значение
+        dist= TCNT1L;
+        dist |= (TCNT1H << 8);
+    }
+}
+
+uint16_t listen_rangefinder()
+{
+    PORTE |= (1<<TRIG);
+    _delay_us(10); // Либо от 15 до 25 мкс 
+    PORTE &= ~(1<<TRIG);
+    _delay_ms(50); // ждём возаращения сигнала 
+/*
+    D= t*V/2=t*340/2 м/с = t*0.34/2 мм/мкс = t/2.9/2 мм/мкс = t/5.8  мм/мкс 
+    T= 1/(11059200/256) = 1/43200 = 23 мкс
+    k = 5.8/23 = 0.25
+    D= t/k = t/0.25 = 4*t мм
+*/
+    return(dist*4);
+}
