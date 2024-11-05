1b44cee753ffcde40255b5bddf5d1adc36a9d620
SergALLy <cfyz70903@mail.ru>
2024-11-05 16:24:48 +0300

    Laba_7

---
+++ Laba_7/inc/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+// Общие библиотеки
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// Пользовательские библиотеки
+#include "adc.h" // Работа с АЦП
+#include "encoder.h" // Работа с энкодером
+
+#endif
